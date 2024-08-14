69be88dcec69fb0219eb884fea63f5e98fba9826
Matt Banack <mbanack@gmail.com>
2024-08-14 17:28:52 -0500

    clippy lint

--- src/lib.rs
+++ src/lib.rs
@@ -17,19 +17,15 @@
 //!
 //! ## Basic usage example
 //! ```
-//! use chex::{Chex,ChexInstance};
+//! let chex: &Chex = Chex::init(true);
+//! let ci_a: ChexInstance = Chex::get_chex_instance();
+//! let ci_b: ChexInstance = chex.get_instance();
 //!
-//! fn main() {
-//!     let chex: &Chex = Chex::init(true);
-//!     let ci_a: ChexInstance = Chex::get_chex_instance();
-//!     let ci_b: ChexInstance = chex.get_instance();
+//! ci_a.signal_exit();
 //!
-//!     ci_a.signal_exit();
-//!
-//!     assert!(ci_b.poll_exit());
-//!     let ci_c = chex.get_instance();
-//!     assert!(ci_c.poll_exit());
-//! }
+//! assert!(ci_b.poll_exit());
+//! let ci_c = chex.get_instance();
+//! assert!(ci_c.poll_exit());
 //! ```
 
 use log::error;
